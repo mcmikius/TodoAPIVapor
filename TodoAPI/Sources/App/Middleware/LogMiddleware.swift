e23a4338f9a7fac949448dc17018eb0287bc8f68
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-13 12:35:02 +0200

    add time to log

--- TodoAPI/Sources/App/Middleware/LogMiddleware.swift
+++ TodoAPI/Sources/App/Middleware/LogMiddleware.swift
@@ -12,9 +12,20 @@ final class LogMiddleware: Middleware {
     }
     
     func respond(to req: Request, chainingTo next: Responder) throws -> Future<Response> {
-        logger.info(req.description)
-        return try next.respond(to: req)
+        let start = Date()
+        return try next.respond(to: req).map { res in
+            self.log(res, start: start, for: req)
+            return res
+        }
     }
+    
+    func log(_ res: Response, start: Date, for req: Request) {
+        let reqInfo = "\(req.http.method.string) \(req.http.url.path)"
+        let resInfo = "\(res.http.status.code) " + "\(res.http.status.reasonPhrase)"
+        let time = Date().timeIntervalSince(start).readableMilliseconds
+        logger.info("\(reqInfo) -> \(resInfo) [\(time)]")
+    }
+    
 }
 
 extension LogMiddleware: ServiceType {
