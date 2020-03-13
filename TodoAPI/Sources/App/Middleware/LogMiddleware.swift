315d9ddcca685918dc1b4bb2c3cbef5aa16424bb
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-13 12:29:32 +0200

    add logMiddleware

--- TodoAPI/Sources/App/Middleware/LogMiddleware.swift
+++ TodoAPI/Sources/App/Middleware/LogMiddleware.swift
@@ -3,17 +3,33 @@
 import Vapor
 
 /// Logs all requests that pass through it.
-final class LogMiddleware {
-  // Your code here
+final class LogMiddleware: Middleware {
+    
+    let logger: Logger
+    
+    init(logger: Logger) {
+        self.logger = logger
+    }
+    
+    func respond(to req: Request, chainingTo next: Responder) throws -> Future<Response> {
+        logger.info(req.description)
+        return try next.respond(to: req)
+    }
+}
+
+extension LogMiddleware: ServiceType {
+    static func makeService(for container: Container) throws -> LogMiddleware {
+        return try .init(logger: container.make())
+    }
 }
 
 extension TimeInterval {
-  /// Converts the time internal to readable milliseconds format, i.e., "3.4ms"
-  var readableMilliseconds: String {
-    let string = (self * 1000).description
-    // include one decimal point after the zero
-    let endIndex = string.index(string.index(of: ".")!, offsetBy: 2)
-    let trimmed = string[string.startIndex..<endIndex]
-    return .init(trimmed) + "ms"
-  }
+    /// Converts the time internal to readable milliseconds format, i.e., "3.4ms"
+    var readableMilliseconds: String {
+        let string = (self * 1000).description
+        // include one decimal point after the zero
+        let endIndex = string.index(string.index(of: ".")!, offsetBy: 2)
+        let trimmed = string[string.startIndex..<endIndex]
+        return .init(trimmed) + "ms"
+    }
 }
